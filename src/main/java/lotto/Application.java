e2e8ae05203d3b41aba05195a8321b182fc31273
YunSeok-Choi <dbstjr9898@naver.com>
2025-11-01 11:15:28 +0900

    feat: 로또 발행 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     static final int LOTTO_PRICE = 1_000;
+    static final int LOTTO_MIN_NUMBER = 1;
+    static final int LOTTO_MAX_NUMBER = 45;
+    static final int LOTTO_NUMBER_COUNT = 6;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -9,5 +18,15 @@ public class Application {
 
     static int calculateTicketCount(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    static Lotto createLotto() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(
+                LOTTO_MIN_NUMBER,
+                LOTTO_MAX_NUMBER,
+                LOTTO_NUMBER_COUNT
+        ));
+        numbers.sort(Integer::compareTo);
+        return new Lotto(numbers);
     }
 }
