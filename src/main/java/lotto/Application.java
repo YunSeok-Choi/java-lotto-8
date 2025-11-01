d96ef0e2fd4eb6aede87c083b599c61a1f847e2f
YunSeok-Choi <dbstjr9898@naver.com>
2025-11-01 11:07:06 +0900

    feat: 구매 금액에 맞는 로또 계산 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
 public class Application {
+    static final int LOTTO_PRICE = 1_000;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    static int calculateTicketCount(int purchaseAmount) {
+        return purchaseAmount / LOTTO_PRICE;
     }
 }
